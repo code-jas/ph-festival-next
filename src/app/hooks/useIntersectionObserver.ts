54239961a9191315df48ff11b325664ebc7bf6b7
Angelo Silvestre <johnangelo.silvestre04@gmail.com>
2024-06-01 19:28:26 +0800

    feat: carousel feature

--- src/app/hooks/useIntersectionObserver.ts
+++ src/app/hooks/useIntersectionObserver.ts
@@ -1,26 +1,26 @@
 import { useEffect, useRef } from "react"
 
 // TODO: RESEARCH ABOUT THIS
-const useIntersectionObserver = (callback: IntersectionObserverCallback, options: IntersectionObserver) => { 
-    const observer = useRef<IntersectionObserver | null>();
-    // console.log('observer :>> ', observer);
 
-    useEffect(() =>  {
-        observer.current = new IntersectionObserver(callback, options);
-        return () => { observer.current?.disconnect()}
-    },[callback, options])
+const useIntersectionObserver = (callback: IntersectionObserverCallback, options: IntersectionObserverInit = {}) => {
+  const observer = useRef<IntersectionObserver | null>(null);
 
-    const observe = (element: HTMLElement) => {
-        observer.current?.observe(element)
-        // console.log('observe:', observer); 
-    }
+  useEffect(() => {
+    observer.current = new IntersectionObserver(callback, options);
+    return () => {
+      observer.current?.disconnect();
+    };
+  }, [callback, options]);
 
-    const unobserver = (element: HTMLElement) => {
-        observer.current?.unobserve(element);
-        // console.log('unobserve :>> ', observer);
-    }
+  const observe = (element: HTMLElement) => {
+    observer.current?.observe(element);
+  };
 
-    return { observe, unobserver }
-}
+  const unobserve = (element: HTMLElement) => {
+    observer.current?.unobserve(element);
+  };
+
+  return { observe, unobserve };
+};
 
 export default useIntersectionObserver;
