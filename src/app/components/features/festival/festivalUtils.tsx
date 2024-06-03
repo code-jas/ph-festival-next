897bcc264c2d09df8e9ffe7514cbdb3a4f8e2a2b
Angelo Silvestre <johnangelo.silvestre04@gmail.com>
2024-06-04 00:19:08 +0800

    the power of any

--- src/app/components/features/festival/festivalUtils.tsx
+++ src/app/components/features/festival/festivalUtils.tsx
@@ -1,21 +1,21 @@
 // utils.js
-export const sortByName = (data) => {
-    return data.sort((a, b) => a.name.localeCompare(b.name));
+export const sortByName = (data: any) => {
+    return data.sort((a:any, b:any) => a.name.localeCompare(b.name));
   };
   
-  export const displayIslandGroupButtons = (data) => {
-    // Implement this function as needed
-  };
+  // export const displayIslandGroupButtons = (data) => {
+  //   // Implement this function as needed
+  // };
   
-  export const displayFestivalItems = (data) => {
-    // Implement this function as needed
-  };
+  // export const displayFestivalItems = (data) => {
+  //   // Implement this function as needed
+  // };
   
-  export const searchButton = () => {
-    // Implement this function as needed
-  };
+  // export const searchButton = () => {
+  //   // Implement this function as needed
+  // };
   
-  export const goingUpBtn = () => {
-    // Implement this function as needed
-  };
+  // export const goingUpBtn = () => {
+  //   // Implement this function as needed
+  // };
   
